982b2fa290590106a529972dfa6afc36e38a1010
Rhenald <renaldkarel@gmail.com>
2022-11-25 15:47:47 +0700

    finish: dynamic routes - implement `Link` to blog post in landing page - delete unnecessary stuff - change `index` site title

--- components/layout.tsx
+++ components/layout.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 import { ReactNode } from 'react';
 
 const name = 'Rhenald Karrel';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'Rhenald Karrel | Blog';
 
 export default function Layout({ children, home }: {
   children: ReactNode,
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
 import type { TPosts } from '../lib/posts';
+import Link from 'next/link';
+import Date from '../components/date';
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -23,21 +25,17 @@ export default function Home({ allPostsData }: { allPostsData: TPosts[] }) {
         <p>
           a growth mindset Software Engineer focused on Frontend Web Development. I am passionate to create a scalable, readable, and efficient code to solve real problems through collaboration and strategic thinking. Sometimes I do design stuffs too.
         </p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
