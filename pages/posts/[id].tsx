864825bd4c350cca4f316b86c342f13cda84cf19
Rhenald <renaldkarel@gmail.com>
2022-11-25 15:39:55 +0700

    polishing the post page - add `date-fns` lib - add `Date` component - add style on post page and semantic html

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,17 +1,28 @@
 import Layout from '../../components/layout';
 import { GetStaticProps, GetStaticPaths } from 'next';
 import { getAllPostIds, getPostData, TPosts } from '../../lib/posts';
+import Head from 'next/head';
+import Date from '../../components/date';
+import utilStyles from "../../styles/utils.module.css";
 
 export default function Post({ postData }: { postData: TPosts }) {
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml}} />
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <header>
+          <h1 className={utilStyles.headingXl}>
+            {postData.title}
+          </h1>
+          <div className={utilStyles.lightText}>
+            <Date dateString={postData.date} />
+          </div>
+        </header>
+        <br />
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml}} />
+      </article>
     </Layout>
   );
 }
